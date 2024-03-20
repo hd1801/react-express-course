415e23109020e4280273b3be1a10baa4775c3d19
hd1801 <hardikdhawan0108@gmail.com>
2024-03-20 23:24:01 +0530

    feat: add client packages

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+import daisy from "daisyui";
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisy],
+  daisyui: {
+    themes: ["lemonade", "night"],
+  },
+};
